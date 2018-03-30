2403961d98166a9194dddb591eb4370c5335b974
杨英强 <cevin15@qq.com>
2018-03-30 14:35:39 +0800

    更新jpa的代码例子

--- 003-spring-data-jpa/jpa-lab/src/main/java/com/youbenzi/UserController.java
+++ 003-spring-data-jpa/jpa-lab/src/main/java/com/youbenzi/UserController.java
@@ -1,5 +1,7 @@
 package com.youbenzi;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +28,17 @@ public class UserController {
 	@GetMapping(path = "/all")
 	public Iterable<User> getAllUsers() {
 		return userRepository.findAll();
+	}
+	
+	@GetMapping(path = "/find")
+	public List<User> findByName(@RequestParam int age) {
+		return userRepository.findByAgeLessThan(age);
+	}
+	
+	@GetMapping(path = "/update")
+	public String update(@RequestParam int age, @RequestParam long id) {
+		userRepository.updateAge(age, id);
+		return "Updated";
 	}
 	
 }
--- 003-spring-data-jpa/jpa-lab/src/main/java/com/youbenzi/UserRepository.java
+++ 003-spring-data-jpa/jpa-lab/src/main/java/com/youbenzi/UserRepository.java
@@ -1,7 +1,25 @@
 package com.youbenzi;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
+	public User findByName(String name);
+
+	public List<User> findByAgeLessThan(int age);
+
+	@Query(value = "select * from user", nativeQuery = true)
+	public List<User> listUsers();
+	
+	@Modifying
+	@Transactional
+	@Query(value = "update user set age = ?1 where id = ?2", nativeQuery = true)
+	public void updateAge(int age, long id);
+	
 }
