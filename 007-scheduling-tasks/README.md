f2c87de916be4fbe303aad3ab495735742977e88
杨英强 <cevin15@qq.com>
2018-04-24 15:55:30 +0800

    定时任务

---
+++ 007-scheduling-tasks/README.md
@@ -0,0 +1,1 @@
+笔记内容：[http://www.youbenzi.com/view/note/1/513](http://www.youbenzi.com/view/note/1/513)
---
+++ 007-scheduling-tasks/scheduling-tasks/src/main/java/com/youbenzi/ScheduledTasks.java
@@ -0,0 +1,24 @@
+package com.youbenzi;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+
+	private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+//    @Scheduled(fixedRate = 5000)
+//    @Schedules(value = {@Scheduled(), @Scheduled()})
+    @Scheduled(cron="0 58 14 * * *")
+    public void reportCurrentTime() {
+        log.info("The time is now {}", dateFormat.format(new Date()));
+    }
+}
